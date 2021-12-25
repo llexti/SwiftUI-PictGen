a9ef410d01dea6a3170c055c36333dba204eabe8
Laurent Lefevre <laurent@neoware.org>
2021-12-25 12:36:37 +0100

    Initial Commit

---
+++ SwiftUI-PictGen/SwiftUI_PictGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PictGenApp.swift
+//  SwiftUI-PictGen
+//
+//  Created by Laurent Lefevre on 25/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PictGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
